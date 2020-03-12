7b7880e4aab91cc531c011c876eedd59680d2eff
Doug M. Harris <dharris@hq.xsilo.com>
2020-03-12 09:43:22 -0400

    Fetch the certs and secrets that MFA needs
    MFA only needs the https cert, the MFAKey, the "salt" and
    the the couchbase password.

--- opt/dist/scripts/azure/keyvault.sh
+++ opt/dist/scripts/azure/keyvault.sh
@@ -29,28 +29,42 @@ mkdir ${KV_DIR}/certs ${KV_DIR}/secrets
 TOKEN=$(curl -s 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -H Metadata:true | extractJSONValue access_token)
 
 # Get the certificates and their private keys
-certs=$(curl -s -H "Authorization: Bearer ${TOKEN}" ${KEYVAULT}/certificates?api-version=${API_VER} \
-   | /opt/node/bin/node -e \
-    "let certs = JSON.parse(require('fs').readFileSync(0, 'utf8')).value; \
-     for (cert of certs) { \
-        console.log(cert.id.split('/').pop()); \
-     }" )
+certs=httpd
 
-## FIXME : Fetch only HTTPS or change keyvault to MFA
-#for cert in $certs ; do
-#   fetchSecret $cert | tee \
-#      >(openssl x509 -outform pem  > ${KV_DIR}/certs/${cert}.crt) \
-#      >(openssl rsa -outform PEM > ${KV_DIR}/certs/${cert}.key) \
-#      >(sed '1,/-----END CERTIFICATE-----/d' > ${KV_DIR}/certs/${cert}.chain) \
-#      > /dev/null
-#   ln -s -f ${KV_DIR}/certs/${cert}.crt /etc/certs/${cert}.crt
-#   ln -s -f ${KV_DIR}/certs/${cert}.chain /etc/certs/${cert}.chain
-#   ln -s -f ${KV_DIR}/certs/${cert}.key /etc/certs/${cert}.key
-#done
+for cert in $certs ; do
+   fetchSecret $cert | tee \
+      >(openssl x509 -outform pem  > ${KV_DIR}/certs/${cert}.crt) \
+      >(openssl rsa -outform PEM > ${KV_DIR}/certs/${cert}.key) \
+      >(sed '1,/-----END CERTIFICATE-----/d' > ${KV_DIR}/certs/${cert}.chain) \
+      > /dev/null
+   ln -s -f ${KV_DIR}/certs/${cert}.crt /etc/certs/${cert}.crt
+   ln -s -f ${KV_DIR}/certs/${cert}.chain /etc/certs/${cert}.chain
+   ln -s -f ${KV_DIR}/certs/${cert}.key /etc/certs/${cert}.key
+done
 
 # Get the Application Insights Instrumentation Key
 fetchSecret InstrumentationKey > ${KV_DIR}/secrets/InstrumentationKey
 
-# TODO: Get the Couchbase password
+# Get the encrption key for MFA
+fetchSecret MFAKey > ${KV_DIR}/secrets/MFAKey
+
+# Get the "salt"
+salt=$(fetchSecret salt)
+if [ "$salt" != "undefined" ] ; then
+   echo "encodeSalt =" $(fetchSecret salt) > ${KV_DIR}/secrets/salt
+   ln -s -f ${KV_DIR}/secrets/salt /etc/gluu/conf/salt
+
+   # Get the Couchbase admin password
+   GCB="/etc/gluu/conf/gluu-couchbase.properties"
+   if [ -f $GCB ]; then
+      # First time. Strip out the password to create a template
+      sed '/^auth.userPassword:/d' $GCB > ${GCB}.template
+   fi
+   cp ${GCB}.template ${KV_DIR}/secrets/couchbaseGluuUserPassword
+   echo "auth.userPassword:" $(fetchSecret couchbaseGluuUserPassword) \
+      >> ${KV_DIR}/secrets/couchbaseGluuUserPassword
+   ln -s -f ${KV_DIR}/secrets/couchbaseGluuUserPassword $GCB
+
+fi
 
 exit 0
